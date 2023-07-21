36ebbf35e868cf498f199f93d05cf93ee3b9f637
XW <xuan.wen@vanderbilt.edu>
2023-07-21 09:46:13 -0500

    quaddle pub 1.0

--- addBeak.py
+++ addBeak.py
@@ -4,43 +4,29 @@ import math
 
 def addBeak(body_type):
     if body_type == 'sphere':
-        loc = (2,0,7)
+        loc = (3,0,7)
     elif body_type == 'vertical_oblong':
-        loc = (2,0,9)
+        loc = (3,0,9)
     elif body_type == 'horizontal_oblong':
-        loc = (2,0,7)
+        loc = (3,0,7)
     elif body_type == 'cube':
-        loc = (2,0,6.5)
+        loc = (3,0,6.5)
     else:
-        loc = (2,0,7)
+        loc = (3,0,7)
     
     # create cylinder object
-    cube = bpy.ops.mesh.primitive_cube_add(location = loc)
-    bpy.context.object.name = "Cube"
-    obj = bpy.data.objects['Cube']
+    bpy.ops.mesh.primitive_cone_add(radius1=1, radius2=1, depth=1,vertices=16, location=loc)
+    bpy.context.object.name = "Beak"
+    obj = bpy.data.objects['Beak']
     mod = obj.modifiers.new('Subsurf', 'SUBSURF')
-    mod.levels = 5 # Set the number of subdivisions
-    mod.render_levels = 5 # Set the number of subdivisions to use for rendering
+    mod.subdivision_type = 'SIMPLE'
+    mod.levels = 3  # Set the number of subdivisions
+    mod.render_levels = 3  # Set the number of subdivisions to use for rendering
+
+
 
     bpy.ops.object.mode_set(mode='EDIT')
-    
-    bpy.ops.transform.resize(value=(0.5, 0.5, 1))
-    me = bpy.context.active_object.data
-    bm = bmesh.from_edit_mesh(me)
-    for face in bm.faces:
-        if abs(face.normal.z) < 0.99:
-            face.select = False
-    for face in bm.faces:
-        if face.select:
-            for edge in face.edges:
-                edge.select == True
-        else:
-            for edge in face.edges:
-                edge.select == False
-                
-    creaseLayer = bm.edges.layers.crease.verify()
-    selectedEdges = [ e for e in bm.edges if e.select ]
-    for e in selectedEdges: e[ creaseLayer ] = 1.0
+    bpy.ops.transform.resize(value=(0.75, 0.75, 1))
     
     bpy.ops.object.mode_set(mode='OBJECT')
     obj.data.update()
